0836a392cbe1f65457af19d5fc7e90e623c5b916
YiZhiYuanYuan-qyy <yq74@duke.edu>
2020-12-04 20:08:19 +0800

    6

---
+++ Homework_week06/transformed screen/src/ofApp.cpp
@@ -0,0 +1,129 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetBackgroundColor(250, 128, 114);
+	vid.load("video.mp4");
+	vid.play();
+	first_fbo.allocate(1920, 1080);
+	second_fbo.allocate(1920, 1080);
+	third_fbo.allocate(1920, 1080);
+	fourth_fbo.allocate(1920, 1080);
+
+	bezManager.setup(10);
+	bezManager.addFbo(&first_fbo);
+	bezManager.addFbo(&second_fbo);
+	bezManager.addFbo(&third_fbo);
+	bezManager.addFbo(&fourth_fbo);
+
+	bezManager.loadSettings();
+	change = false;
+	back = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+	vid.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofClear(250, 128, 114);
+	if (change == false&&back == true) {
+		first_fbo.begin();
+		vid.draw(0, 0, 1920, 1080);
+		first_fbo.end();
+
+		second_fbo.begin();
+		vid.draw(0, 0, 1920, 1080);
+		second_fbo.end();
+
+		third_fbo.begin();
+		vid.draw(0, 0, 1920, 1080);
+		third_fbo.end();
+
+		fourth_fbo.begin();
+		vid.draw(0, 0, 1920, 1080);
+		fourth_fbo.end();
+
+		bezManager.draw();
+	}
+	if (change == true&&back == false) {
+		vid.draw(0, 0, 1920, 1080);
+	}
+
+
+	string _help = "[ENTER] key : Toggle guide visible.\n";
+	_help += "[s] key : Save settings.\n";
+	_help += "[l] key : Load settings.\n";
+	_help += "Left drag the control point : Move Corners/Anchors.\n";
+	_help += "Right click the control point : Toggle bezier/homoglaphy mode.\n";
+	_help += "Arrow key : Move Corners/Anchors. (selected control point)\n";
+	_help += "[b] key : watch transformed video\n";
+	_help += "[c] key : watch full screen video\n";
+	ofDrawBitmapString(_help, 10, 20);
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	bezManager.keyPressed(key);
+	if (key == OF_KEY_RETURN) {
+		bezManager.toggleGuideVisible();
+	}
+	if (key == 's') {
+		bezManager.saveSettings();
+	}
+	if (key == 'l') {
+		bezManager.loadSettings();
+	}
+	if (key == 'c') {
+		change = true;
+		back = false;
+	}
+	if (key == 'b') {
+		change = false;
+		back = true;
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+	bezManager.mouseDragged(x, y, button);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+	bezManager.mousePressed(x, y, button);
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
